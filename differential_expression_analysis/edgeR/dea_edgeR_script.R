0ae55d74b0b68a303ff90282a3419b1d368c0ebd
DimAnge <angeldim1998@gmail.com>
2024-08-12 18:26:46 +0300

    Continued working on the DEA R script. Performed PCA analysis and Differential Expression Analysis with edgeR, Need to work on annotation and visualization

--- differential_expression_analysis/edgeR/dea_edgeR_script.R
+++ differential_expression_analysis/edgeR/dea_edgeR_script.R
@@ -73,5 +73,75 @@ write.table(norm_d, file ="data/GSE193754_cpm.txt", sep = '\t')
 write.table(log_cpm, file ="data/GSE193754_logcpm.txt", sep = '\t')
 
 
-##Performing a Principal Component Analysis
+##Performing a Principal Component Analysis with FactorMineR (just for practice/small sample number)
+#Transposing the table of logcpm
+trans_log_cpm <- t(log_cpm)
 
+#PCA (keeping the scale.unit since we have removed most lowly expressed genes)
+pca_cpm <- PCA(trans_log_cpm,scale.unit = T, graph = F)
+
+#Extract the coordinates from the PCA data object
+pca_cord <- as.data.frame(pca_cpm$ind$coord)
+
+#Variance explained % by each component in pca_cpm$eig
+pca_perc <- pca_cpm$eig[,2]
+
+#Plotting the PCA with ggplot2 using the first 2 Principal Components
+pca_plot <- ggplot(pca_cord, aes(pca_cord[,1], pca_cord[,2],
+       color = meta$sample_type))+
+  geom_point(key_glyph = "point") +
+  labs(title = "PCA - GSE193754",
+       x = paste0("PC1(", round(pca_perc[1], 2), "%)"),
+       y = paste0("PC2(", round(pca_perc[2], 2), "%)"),
+       color = 'Sample Type') +
+  theme(plot.title = element_text(hjust = 0.5))
+print(pca_plot)
+
+#Exporting plot
+pdf(file = 'Plots/PCA_GSE193754.pdf', width = 8, height = 6)
+print(pca_plot)
+dev.off()
+
+##Differential Expression Analysis
+#Estimating Dispersion (in edgeR : GLMCommonDisp, GLMTrendedDisp, GLMTagwiseDisp )
+dd1 <- estimateDisp(dd, design, verbose = TRUE)
+
+#Fitting the model (estimates the effect of each variable and evaluate the logFC)
+fit <- glmQLFit(dd1, design)
+
+#Comparison between  MSH2-deficient vs wild type  (create contrast)
+colnames(design) <- c("grouphMSH2_deficient","groupwild_type")
+contr <- makeContrasts(grouphMSH2_deficient - groupwild_type, levels = design)
+
+#Running DE test
+deg_lrt <- glmQLFTest(fit, contrast = contr)
+head(deg_lrt$table)
+
+##False Discovery Rate Correction (Benjamin - Hochberg) - Multiple comparison adjustment
+#Adding fdr
+deg_lrt$table$fdr <- p.adjust(deg_lrt$table$PValue, method = "BH")
+
+##Filtering results
+#fdr < 0.05 and absolute lofFC >= 0.58 (strict 0.01 and 1)
+top <- deg_lrt$table[c(deg_lrt$table$fdr <= 0.05) & abs(deg_lrt$table$logFC) >= 0.58,] #34 genes
+
+#Exporting Results
+write.xlsx(top, file = "data/DE_Results_GSE193754_FDR_0_05_logFC_0_58.xlsx", rowNames = TRUE, colNames = TRUE)
+
+#Saving workspace
+save.image("data/GSE193754_DE_results.Rdata")
+
+#Annotating results wit biomart (from the 16786 genes)/ some genes maybe pseudogenes or some mutated genes need other database
+mart <- useEnsembl(biomart = 'ensembl',
+                   dataset = 'hsapiens_gene_ensembl',
+                   version = 108)
+annot <- getBM(filters = "external_gene_name",
+               attributes = c("ensembl_gene_id",
+                              "hgnc_symbol",
+                              "description",
+                              "start_position",
+                              "end_position",
+                              "strand",
+                              "external_gene_name"),
+               values = rownames(deg_lrt$table),
+               mart = mart)
